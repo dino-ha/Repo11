9e3901f0da0523a232aba8848c2d21bf452cf45f
dino-ha <hadzirovicdino@gmail.com>
2022-04-08 03:07:26 +0200

    Add files via upload

--- ex12/src/index.js
+++ ex12/src/index.js
@@ -1,1 +1,17 @@
+function splittingArrayInGroups(arr, n){
 
+    var z = n;
+    var result = [];
+    for (var x = 0; x < arr.length; x++){
+
+        for (n; n > 0; n--){
+
+            result.push(arr[n]);
+        }
+        n = z;
+    }
+}
+
+splittingArrayInGroups();
+
+module.exports = splittingArrayInGroups;
