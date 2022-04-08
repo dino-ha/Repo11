3f415bf2f029fd5a328e948e836603f1e07b782f
dino-ha <hadzirovicdino@gmail.com>
2022-04-08 03:06:28 +0200

    Add files via upload

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -1,1 +1,14 @@
+function myBouncer(arr){
 
+    arr.split();
+    for (var x = 0; x < arr.length; x++){
+
+        if (arr[x] == false || arr[x] == null || arr[x] == 0 || arr[x] == "" || arr[x] == undefined || arr[x == NaN]){
+
+            arr.shift(arr[x]);
+        }
+    }
+}
+
+myBouncer(arr);
+module.exports = myBouncer;
