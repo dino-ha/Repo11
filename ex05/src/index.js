d4a59652fa0dce0fe60d2989310a752e6e675929
dino-ha <hadzirovicdino@gmail.com>
2022-04-08 03:03:56 +0200

    Add files via upload

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,1 +1,15 @@
+var monitorsListArray = ["Apple", "Peach", "Berry"];
 
+var monitorsList = [];
+
+function myMonitorsFunction(){
+
+    for (var i; i < monitorsListArray.length; i++){
+
+        monitorsList.push(monitorsListArray[i]).push(i);
+    }
+
+    return monitorsList;
+}
+
+module.exports = myMonitorsFunction;
