7bbc9346ca5d52cc135f607bb7fc9bde38c9c5dc
dino-ha <hadzirovicdino@gmail.com>
2022-04-08 03:05:31 +0200

    Add files via upload

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,1 +1,24 @@
+function largestNumFromArr(arr){
 
+    maxNumArray = [];
+    var largestNum = 0;
+
+    for (x = 0; x < arr.length; x++){
+
+        for (y = 0; y < arr.length; y++){
+
+            if (arr[x][y] > largestNum){
+
+                largestNum = arr[x][y];
+                
+            }
+        
+        }
+        maxNumArray.push(largestNum);
+        largestNum = 0;
+    }
+
+    return maxNumArray;
+}
+
+module.exports = largestNumFromArr;
