0543c6a82637bf1fd77626177ddacd1d247aa018
dino-ha <hadzirovicdino@gmail.com>
2022-04-08 03:05:59 +0200

    Add files via upload

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,1 +1,30 @@
+function myMutation(arr){
 
+    var sameLetters;
+    arr.split();
+    for (var x = 0; x < arr.length; x++){
+
+        if (arr.search(/arr[x]/i)){
+
+            sameLetters = true;
+        }
+        else {
+
+            sameLetters = false;
+            
+        }
+    }
+    if (sameLetters == true){
+
+        return true;
+    }
+    else {
+
+        return false;
+    }
+    
+}
+
+myMutation();
+
+module.exports = myMutation;
