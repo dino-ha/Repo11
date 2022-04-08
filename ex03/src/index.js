84f52a17f627a16e6428675f5671a4f0841ee37c
dino-ha <hadzirovicdino@gmail.com>
2022-04-08 03:02:48 +0200

    Add files via upload

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -1,1 +1,22 @@
+myPetsArray = ["Dog", "Cat"];
 
+function myArrayFunction(myPets){
+
+    myNewPets = [];
+    myNewPets.push("Bird", "Fish");
+    myNewPets.unshift("Lion");
+    myNewPets.pop();
+    myNewPets.pop();
+    myNewPets.push("Cat", "Bird");
+    firstPet = myNewPets[0];
+    lastPet = myNewPets[myNewPets.length-1];
+    return myNewPets;
+}
+
+myArrayFunction(myPetsArray);
+
+console.log(firstPet);
+console.log(lastPet);
+console.log(myNewPets);
+
+module.exports = myArrayFunction;
