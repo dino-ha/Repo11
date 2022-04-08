140c4dc6562fe5c040a981c9370f77d5f178b350
dino-ha <hadzirovicdino@gmail.com>
2022-04-08 03:03:24 +0200

    Add files via upload

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,1 +1,33 @@
+var temps = [
+
+    [31, 32, 19, 37],
+    [29, 27, 55, 36],
+    [17, 27, 42, 46],
+    [29, 52, 21, 64],
+    [91, 27, 31, 61] 
+];
+
+var averageDayTemp = [];
+
+function myArrayFunction(tempArr){
+
+    var sumy = 0;
+    for (var x = 0; x < tempArr.length; x++){
+
+        for (var y = 0; y < tempArr.length; y++){
+
+            sumy += tempArr[y];
+        }
+        
+        sumy = (sumy/tempArr.length);
+        averageDayTemp.push(sumy);
+        sumy = 0;
+    }
+    console.log(averageDayTemp);
+
+    return averageDayTemp;
+}
+
+myArrayFunction(temps);
+module.exports = myArrayFunction;
 
