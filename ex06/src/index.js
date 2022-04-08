05275fec068b53193c32d6828ce252f7d9b50062
dino-ha <hadzirovicdino@gmail.com>
2022-04-08 03:04:28 +0200

    Add files via upload

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,1 +1,9 @@
+var myNestedArray = [["Toblerone", 5], ["Milka", 3]];
 
+function myNestedFunction(arr){
+
+    return myNestedArray;
+}
+
+myNestedFunction(myNestedArray);
+module.exports = myNestedFunction;
