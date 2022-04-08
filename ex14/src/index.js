bc10cb682609a387ebed2c19b6ed94367469570e
dino-ha <hadzirovicdino@gmail.com>
2022-04-08 03:08:34 +0200

    Add files via upload

--- ex14/src/index.js
+++ ex14/src/index.js
@@ -1,1 +1,27 @@
+var myArr = [2, 5, 9, 8, 2];
 
+function bubbleSort(arr){
+
+    var largestNum = 0;
+    var z = 0;
+    for (var x = arr.length-1; x >= 0; x--){
+    
+        for (var y = 0; y < arr.length-z; y++){
+
+            if (arr[y] > largestNum){
+
+                largestNum = arr[y];
+            }
+        }
+
+        arr[x] = largestNum; 
+        z++;
+
+    }
+
+    return arr;
+}
+
+bubbleSort(myArr);
+
+module.exports = bubbleSort;
