670f4b7889fbc7835b11d0240706a47de1f82421
dino-ha <hadzirovicdino@gmail.com>
2022-04-08 03:06:55 +0200

    Add files via upload

--- ex11/src/index.js
+++ ex11/src/index.js
@@ -1,1 +1,14 @@
+function mySplice(arr1, arr2, n){
 
+    arr1_temp = arr1;
+    arr2_temp = arr2;
+
+    for (n; n < arr2.length; n++){
+
+        arr2[n] = arr1.splice(n, 0, arr1[n]);
+    }
+}
+
+mySplice(arr1, arr2, n);
+
+module.exports = mySplice;
