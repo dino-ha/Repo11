30f0e15a9d1f3035fc42e02992fe5ebb4b2f5fe0
dino-ha <hadzirovicdino@gmail.com>
2022-04-08 03:08:03 +0200

    Add files via upload

--- ex13/src/index.js
+++ ex13/src/index.js
@@ -1,1 +1,20 @@
+var str = "Paragon";
 
+function myReverse(str){
+
+    var reverseString = [];
+    
+    for (var x = str.length-1; x >= 0; x--){
+
+        reverseString.push(str[x]);
+    }
+
+    reverseString = reverseString.join();
+    
+    return reverseString;
+}
+
+console.log(myReverse(str));
+
+
+module.exports = myReverse;
