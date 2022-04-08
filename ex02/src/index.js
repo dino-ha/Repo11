d937f93c2fefd8943582362adec047591421081a
dino-ha <hadzirovicdino@gmail.com>
2022-04-08 03:02:16 +0200

    Add files via upload

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,1 +1,24 @@
+myStr = "Paragon is the best!";
 
+function useMethod(str){
+
+    myRev = str.split("").reverse().join("");
+    countChar = str.split("").reverse().join("");
+    countChar = countChar.split(" ");
+    countChar = countChar.map(countChar => countChar.length);
+    
+
+    const Obj = {
+
+        myReverse: myRev,
+        countCharacters: countChar
+    }
+
+    console.log(Obj);
+    console.log(Obj.countCharacters);
+    return Obj;
+}
+
+useMethod(myStr);
+
+module.exports = useMethod;
